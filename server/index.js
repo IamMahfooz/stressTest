7973fed2b9151572985db749619e60bb6a11e1f1
Mahfooz Alam <mahfooz720946@gmail.com>
2024-03-19 21:48:23 +0530

    feat: retreive and run testcase [still err with fs]

--- server/index.js
+++ server/index.js
@@ -1,22 +1,91 @@
 const cheerio = require('cheerio')
 const express = require('express')
 const app =express()
+const cors = require('cors');
+const fs = require('node:fs');
+const {exec} = require('child_process');
+const _ = require('lodash');
+// path = require('path'),
 
+    app.use(cors())
+app.use(express.json())
 app.use(express.static("../fronted/build"))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../fronted/build", "index.html"));
 })
-app.get('/problem',(req, res)=>getSubmissionCode(req,res))
+app.get('/problem',(req, res)=>getSubmissionCode(req,res));
+app.post('/getMaps',(req,res)=>getFailingCases(req,res));
 app.listen(5000);
 const getSubmissionCode = async (req,res) => {
-    console.log(req.query.param);
-    // get html text from reddit
-    const response = await fetch(req.query.param);
-    // using await to ensure that the promise resolves
-    const body = await response.text();
-    const $=cheerio.load(body)
-    const submittedCode=$("#submission-code").html();
-    res.send(submittedCode);
+    try {
+        console.log(req.query.param);
+        // get html text from reddit
+        const response = await fetch(req.query.param);
+        // using await to ensure that the promise resolves
+        const body = await response.text();
+        const $=cheerio.load(body);
+        const submittedCode= _.unescape($("#submission-code").html());
+        // console.log(submittedCode)
+        writeCodetoFile(submittedCode);
+        compileBinary();
+        res.send(submittedCode);
+    }catch (error){
+        console.log(error)
+    }
 
 };
-// getSubmissionCode();
+const getFailingCases = (req,res)=>{
+    let jsonData = req.body
+    jsonData=jsonData.inLine;
+    const data ={
+        message: "received",
+        alert: "post successfully"
+    }
+    res.send(JSON.stringify(data));
+    executeDiffs(jsonData);
+}
+function executeDiffs(submissionData){
+    const contestNumber = submissionData.contestNumber;
+    const problemIndex = submissionData.problemIndex.toUpperCase();
+    const folderPath = "../../../atcoder_testcases/"+contestNumber+"/"+problemIndex+"/in/";
+    const submittedCode =submissionData.submittedCode;
+    const files=fs.readdirSync(folderPath);
+    let userOutput =[]
+
+    for(let i=0;i<files.length;i++){
+        // console.log(`${i} file was ${files[i]}`);
+        exec(`cat ${folderPath}${files[i]} | ./a.out  > out.txt` ,
+            function (error, stdout, stderr) {
+                if (error !== null) {
+                    console.log('exec error2: ' + error);
+                }
+            });
+        // below reads doesn't works
+        const filePath = __dirname+`/out.txt`
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                // return;
+            }else{
+                console.log(`data from first file was ${data}`)
+                // userOutput[i]=data;
+            }
+        });
+        console.log(`completed file number ${i}`);
+    }
+    // console.log(userOutput);
+}
+function writeCodetoFile(submittedCode){
+    fs.writeFile('Output.cpp', submittedCode, (err) => {
+        if (err) throw err;
+    });
+}
+function compileBinary(){
+    exec('g++ Output.cpp ' ,
+        function (error, stdout, stderr) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+
+}
