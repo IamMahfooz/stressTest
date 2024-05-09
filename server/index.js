520a9512323a48e151bf4f3b608c65bf8dccb4f5
mahfooz <mahfooz720946@gmail.com>
2024-05-09 14:35:54 +0530

    fix: minor issues
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- server/index.js
+++ server/index.js
@@ -5,17 +5,19 @@ const cors = require('cors');
 const fs = require('node:fs');
 const execSync = require('child_process').execSync;
 const _ = require('lodash');
-// path = require('path'),
+const {join} = require("path");
 
-    app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.static("../frontend/build"))
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(join(__dirname, "../frontend/build", "index.html"));
 })
 app.get('/problem',(req, res)=>getSubmissionCode(req,res));
 app.post('/getMaps',(req,res)=>getFailingCases(req,res));
 app.listen(5000);
+
 const getSubmissionCode = async (req,res) => {
     try {
         console.log(req.query.param);
@@ -26,8 +28,8 @@ const getSubmissionCode = async (req,res) => {
         const $=cheerio.load(body);
         const submittedCode= _.unescape($("#submission-code").html());
         // console.log(submittedCode)
-        writeCodeToFile(submittedCode);
-        compileBinary();
+        // writeCodeToFile(submittedCode);
+        // compileBinary();
         res.send(submittedCode);
     }catch (error){
         console.log(error)
@@ -36,6 +38,7 @@ const getSubmissionCode = async (req,res) => {
 };
 const getFailingCases = (req,res)=>{
     let jsonData = req.body
+    // console.log(jsonData.outLine);
     jsonData=jsonData.inLine;
     const data ={
         message: "received",
@@ -45,24 +48,37 @@ const getFailingCases = (req,res)=>{
     executeDiffs(jsonData);
 }
 function executeDiffs(submissionData){
-    const contestNumber = submissionData.contestNumber;
+    const testcaseBool = submissionData.testCaseNumbers;
+    const inLine = submissionData.inLine;
+    const outLine = submissionData.outLine;
+    const contestNumber = submissionData.contestNumber.toUpperCase();
     const problemIndex = submissionData.problemIndex.toUpperCase();
-    const folderPath = "../../../atcoder_testcases/"+contestNumber+"/"+problemIndex+"/in/";
+    const folderPath = "../../../atcoder_testcases/"+contestNumber+"/"+problemIndex;
     const submittedCode =submissionData.submittedCode;
-    const files=fs.readdirSync(folderPath);
-    let userOutput =[]
+    const files=fs.readdirSync(folderPath+"/in/");
+
+    writeCodeToFile(submittedCode);
+    compileBinary();
+
+    let userOutput = ""
+    let sysOutput = ""
 
     for(let i=0;i<files.length;i++){
         // console.log(`${i} file was ${files[i]}`);
-        execSync(`cat ${folderPath}${files[i]} | ./a.out  > out.txt`)
-        // below reads doesn't works
+        execSync(`cat ${folderPath}"/in/"${files[i]} | ./a.out  > out.txt`)
         const filePath = __dirname+`/out.txt`
         userOutput[i]=fs.readFileSync(filePath,
+            {encoding: 'utf8', flag: 'r'});
+        sysOutput[i]=fs.readFileSync(folderPath+"/out/"+files[i],
             {encoding: 'utf8', flag: 'r'});
         // console.log(`the data of ${i} is ${data}`)
         console.log(`completed file number ${i}`);
     }
     console.log(userOutput[0]);
+    console.log(sysOutput[0]);
+    // console.log(testcaseBool)
+    // console.log(inLine);
+    // console.log(outLine);
 }
 function writeCodeToFile(submittedCode){
     try {
