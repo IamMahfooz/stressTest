65c1bf1015183a5762ee39dcf56cb00c336994a3
mahfooz <mahfooz720946@gmail.com>
2024-09-26 16:02:35 +0530

    server: add server for webscrapping
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

---
+++ backend/subCode/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cheerio = require('cheerio');
+const _ = require('lodash');
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/getsubinfo',(req,res)=>getSubInfo(req,res));
+app.listen(5002);
+
+const getSubInfo = async(req,res)=>{
+    try{
+        const url = req.query.param;
+        const response =await fetch(url);
+        const body = await response.text();
+        const $ = cheerio.load(body);
+        const submittedCode = _.unescape($("#submission-code").html());
+
+        const match = url.match(/\/contests\/([^/]+)\//);
+        contestNumber = match[1];
+        const taskLinkSelector = `a[href*="/tasks/${contestNumber}_"]`;
+        const taskLink = $(taskLinkSelector).attr('href');
+        const problemId = taskLink.split('_').pop();
+        res.json({
+            cid: contestNumber,
+            pid: problemId,
+            ucode: submittedCode
+        });
+
+    }catch(error){
+        console.log("error while handling request : ", error)
+    }
+}
