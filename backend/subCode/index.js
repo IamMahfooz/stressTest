644930155bd8edb7e5d37457be7365cd69cac78d
mahfooz <mahfooz720946@gmail.com>
2024-09-26 16:23:20 +0530

    feat : final test success of  web scrapping features
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/subCode/index.js
+++ backend/subCode/index.js
@@ -1,5 +1,11 @@
+// server for webscrapping
+
+// Example to use : curl -X POST
+// "http://localhost:5002/getsubinfo?param=https://atcoder.jp/contests/arc171/submissions/50079569"
+
 const express = require('express');
 const app = express();
+var cors = require('cors')
 const cheerio = require('cheerio');
 const _ = require('lodash');
 
@@ -7,11 +13,13 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/getsubinfo',(req,res)=>getSubInfo(req,res));
+console.log(" starting the server at port : 5002")
 app.listen(5002);
 
 const getSubInfo = async(req,res)=>{
     try{
         const url = req.query.param;
+        console.log(url)
         const response =await fetch(url);
         const body = await response.text();
         const $ = cheerio.load(body);
