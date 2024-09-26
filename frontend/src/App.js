a47d6c7af15e17abc7be4e8aa2c78fdbafefb053
mahfooz <mahfooz720946@gmail.com>
2024-09-26 15:56:55 +0530

    refactor: refactor directory str for v2
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- frontend/src/App.js
+++
@@ -1,7 +0,0 @@
-import Stresstest from "./components/authenticated/stresstest";
-
-export default function App(){
-    return(
-        <Stresstest />
-    )
-}
--- frontend/src/components/status.js
+++
@@ -1,3 +0,0 @@
-export default function Status(){
-    return <h1 style={{textAlign: "center"}}>"Check the last Ticket"</h1>
-}
