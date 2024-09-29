7273a8e5bb6c463a98b80c8d4a1c78ab72f41bca
mahfooz <mahfooz720946@gmail.com>
2024-09-30 00:11:50 +0530

    demo: add demo curl command
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/cs1/main.go
+++ backend/cs1/main.go
@@ -8,50 +8,16 @@ import (
 )
 
 func main() {
-	//_, err := utils.FetchTestcases("2019ddccqual", "A", 20119)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
 
-	//inbytes, err := os.ReadFile("./testcases_20119/in/in01.txt")
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Println("the string was : ", inbytes)
-	////truncatedBytes = inbytes[:]
-	//fmt.Println(strings.IndexByte(string(inbytes), 10))
-	//newByte := inbytes[strings.IndexByte(string(inbytes), 10)+1:]
-	//fmt.Println("the string was : ", string(newByte))
-
-	// testing compile
-	//subCode := "#include\"bits/stdc++.h\"\nusing namespace std;\n#define int long long\nconst int maxn = 100010;\nint T,n,a,b;\nsigned main(){\n\tcin>>T;\n\twhile(T--){\n\t\tcin>>n>>a>>b;\n\t\tif(a>=n/2){\n\t\t\tputs(b>(n-a)*(n-a)?\"No\":\"Yes\");\n\t\t}else{\n\t\t\tint Sp=n-a*2;\n\t\t\tputs(b>a*(n-a)+(a+Sp)*((Sp+1)/2)?\"No\":\"Yes\");\n\t\t}\n\t}\n}"
-	//_, err := utils.CompileSubmission(subCode, 20119)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//_, err := utils.FetchTestcases("ARC171", "A", 20119)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-
-	//testing execution and processing of testcases
-	//bPath := "/home/mahfooz/Desktop/stress-testV2/backend/cs1/compiled_binary20119"
-	//testDirPath := "/home/mahfooz/Desktop/stress-testV2/backend/cs1/testcases_20119"
-	//fmaps := utils.FTestMaps{}
-	//req := utils.Request{
-	//	CID:                 "ARC171",
-	//	PID:                 "A",
-	//	SubCode:             "",
-	//	FirstLineIsNumTests: true,
-	//	NumLinesPerTestCase: 1,
-	//	NumLinesPerOutput:   1,
-	//}
-	//err := utils.ExecuteTestCases(bPath, testDirPath, &req, &fmaps)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
+	//curl command to test the full feature
+	//curl -H 'Content-Type: application/json' -X POST http://localhost:5001/compile -d '{
+	//"cid": "ARC171",
+	//	"pid": "A",
+	//	"ucode": "#include<bits/stdc++.h>\nusing namespace std;\n#define int long long\nconst int maxn = 100010;\nint T,n,a,b;\nsigned main(){\n\tcin>>T;\n\twhile(T--){\n\t\tcin>>n>>a>>b;\n\t\tif(a>=n/2){\n\t\t\tputs(b>(n-a)*(n-a) ? \"No\" : \"Yes\");\n\t\t}else{\n\t\t\tint Sp=n-a*2;\n\t\t\tputs(b>a*(n-a)+(a+Sp)*((Sp+1)/2) ? \"No\" : \"Yes\");\n\t\t}\n\t}\n}",
+	//	"firstLineIsNumTests": true,
+	//	"numLinesPerTestCase": 1,
+	//	"numLinesPerOutput": 1
+	//}'
 
 	//Step 0: Start the server
 	e := echo.New()
@@ -61,5 +27,5 @@ func main() {
 		fmt.Println("error while starting server : ", err.Error())
 		return
 	}
-	fmt.Println("Starting the server at : 5001")
+	//fmt.Println("Starting the server at : 5001")
 }
