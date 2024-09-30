873aca3887d302f5bad8deab3e362773b0d07b9e
mahfooz <mahfooz720946@gmail.com>
2024-09-30 23:42:36 +0530

    feat: add api gateway server to handle worker servers
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/apiGate/main.go
+++ backend/apiGate/main.go
@@ -3,17 +3,27 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
-
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 )
 type Request struct {
-	SubmissionLink      string `json:"submissionLink"`
+	CID                 string `json:"cid"`
+	PID                 string `json:"pid"`
+	SubCode             string `json:"ucode"`
 	FirstLineIsNumTests bool   `json:"firstLineIsNumTests"`
 	NumLinesPerTestCase int    `json:"numLinesPerTestCase"`
 	NumLinesPerOutput   int    `json:"numLinesPerOutput"`
 }
+type FTestMaps []struct {
+	Input        string `json:"in"`
+	SystemOutput string `json:"sOut"`
+	UserOutput   string `json:"uOut"`
+}
+var workNumber int =0
 func main(){
 	// already has submission link , problem id , contest id , TestCaseLine , numInputLine , numOutputLine
 	e :=echo.New()
@@ -22,14 +32,33 @@ func main(){
 
 }
 func workerServers(c echo.Context)error{
-	body :=c.Request().Body
-	fmt.Println(body)
-	resp,err := http.NewRequest("POST","http://locahhost:5001/compile",body)
+	var workLink string
+	if workNumber==0{
+		workLink="https://atcoder-stress-test-v2.onrender.com/compile"
+		workNumber=1
+	}else{
+		workLink="https://stress-test-v2-worker-2.onrender.com/compile"
+		workNumber=0
+	}
+	b, _ := io.ReadAll(c.Request().Body)
+	fmt.Println(workLink)
+
+	fmt.Println(string(b))
+	req,err := http.NewRequest("POST",workLink,bytes.NewBuffer(b))
 	if err != nil{
 		fmt.Println("error found ",err)
 	}
-	respBody := resp.Body
+	req.Header.Add("Content-Type","application/json")
+	client :=&http.Client{}
+	resp,err := client.Do(req)
+	if err!=nil{
+		fmt.Println("failed to make post request from apiGateway ",err)
+	}
 	defer resp.Body.Close()
-
-	return c.JSON(http.StatusOK,respBody)
+	post := &FTestMaps{}
+	derr := json.NewDecoder(resp.Body).Decode(post)
+	if derr != nil {
+		fmt.Println("error while decoding final results ",derr)
+	}
+	return c.JSON(http.StatusOK,post)
 }
