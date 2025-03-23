968ee19c086052eafaf26ebabdc1a6488fec7581
mahfooz <mahfooz720946@gmail.com>
2025-03-23 20:31:21 +0530

    update: atcoder stopped publishing testcases
    Signed-off-by: mahfooz <mahfooz720946@gmail.com>

--- backend/cs1/utils/testFetch.go
+++ backend/cs1/utils/testFetch.go
@@ -14,8 +14,10 @@ func FetchTestcases(contestID, problemID string, uIdentify int) (string, error) 
 	// Fetch the test cases from the server and return the directory path
 
 	// make a database to fetch urlId according to contestID ; every problem id has its own unique link identifier
-	problemUniqueId := "AABE4bXm7hDlF4CZt0DfUtL-a" // ARC171-A
-	finalUrl := "https://www.dropbox.com/sh/nx3tnilzqz7df8a/" + problemUniqueId + "/" + contestID + "/" + problemID + "?dl=1"
+	// ---- fixing the final url untill atcoder starts publishing the testcases again.
+	// problemUniqueId := "AABE4bXm7hDlF4CZt0DfUtL-a" // ARC171-A
+	// finalUrl := "https://www.dropbox.com/sh/nx3tnilzqz7df8a/" + problemUniqueId + "/" + contestID + "/" + problemID + "?dl=1"
+	finalUrl := "https://www.dropbox.com/scl/fo/ctbefh5jjf8uwx6i5dbuy/AH9IK06EAVcGc39oWTCsUek?rlkey=220o6ubtbw308rjs0tj6g8ig1&st=llqs8kga&dl=1"
 	fmt.Println("final url was : ", finalUrl)
 	testCaseFolder := "./" + strconv.Itoa(uIdentify) + ".zip"
 	err := downloadFile(testCaseFolder, finalUrl)
